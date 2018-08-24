ae726e4f0153eda74e03cafd244995f1bf658992
Jeferson Inácio <jeferson.i.silva@gmail.com>
2018-08-24 19:33:46 -0300

    Update vuel.js

--- dist/vuel.js
+++ dist/vuel.js
@@ -1,37 +1,42 @@
-function Vuel(tag, params) {
-	params = typeof params=="object"? params: {};
-	params.data = typeof params.data=="object"? params.data: {};
-	var templateHTML = (document.currentScript? document.currentScript.ownerDocument.getElementById(tag).innerHTML: "");
-	var proto = Object.create(HTMLElement.prototype);
+(function(v) {
+	v.vuel = function(tagname, params) {
+		var doc = document.currentScript.ownerDocument;
+		var style = doc.querySelector('style')||false;
+		style? document.head.appendChild(style): false;
 
+		var _customElementApply = function(wrapper, params) {
+			var template = doc.getElementById(tagname);
+			wrapper.innerHTML = '<div>'+ template.innerHTML +'</div>';
+			params.el = wrapper.children[0];
 
-	proto.createdCallback = function() {};
-	proto.attachedCallback = function() {
-
-		if (typeof params.data.attr=="object") {
-			for(var i in params.data.attr) {
-				var val =(this.getAttribute(i) || params.data.attr[i]);
-				try { eval('val='+val); } catch(e) {}
-				params.data.attr[i] = val;
+			var paramsData = params.data;
+			if (typeof params.data == "function") {
+				paramsData = params.data();
 			}
+			paramsData = JSON.parse(JSON.stringify(paramsData));
+			for(var i in paramsData) {
+				var attr = wrapper.getAttribute(i)||'';
+				if (attr) {
+					try { eval('attr='+attr); } catch(e) {}
+					paramsData[i] = attr;
+				}
+			}
+
+			params.data = paramsData;
+			params.methods = params.methods||{};
+			for(var i in params.methods) { wrapper[i] = params.methods[i]; }
+			var vm = new Vue(params);
+		};
+
+
+		class NewElement extends HTMLElement {
+			constructor() { super(); };
+			createdCallback() { };
+			attachedCallback() { _customElementApply(this, params); };
+			detachedCallback() { };
+			attributeChangedCallback(attr, oldVal, newVal) { };
 		}
 
-		if (this.getAttribute("template")) {
-			templateHTML = document.getElementById( this.getAttribute("template") ).innerHTML;
-		}
-
-		templateHTML = templateHTML.replace(/\<content\>\<\/content\>/g, this.innerHTML);
-		this.innerHTML = "<div>"+ templateHTML +"</div>";
-
-		params.el = this.children[0];
-		var app = new Vue(params);
-		
-		for(var i in (params.methods||[])) {
-			console.log(i);
-			proto[i] = app[i];
-		}
-	};
-
-
-	return document.registerElement(tag, {prototype: proto});
-}
+		// customElements.define(tagname, NewElement);
+		document.registerElement(tagname, NewElement);	};
+})(Vue);
